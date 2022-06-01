3b07ce0ca627ecfaf4ee6a669b6ef95fc0cb39f8
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-06-01 18:06:05 -0400

    defined linked list class with all basic functionalities. add, delete, find, print, etc

--- linked_lists.py
+++ linked_lists.py
@@ -11,10 +11,21 @@ class SinglyLinkedList:
     def get_index(self, target_index):
         index = 0
         cur = self.head
+        if target_index > self.get_count():
+            return -1
         while target_index != index:
             cur = cur.next 
             index+=1
         print(cur.val)        
+        
+       
+    def get_count(self):
+        temp = self.head
+        count = 0
+        while (temp):
+            count+=1
+            temp = temp.next 
+        return count
         
         
     def list_print(self):
@@ -24,13 +35,13 @@ class SinglyLinkedList:
             cur = cur.next 
        
        
-    def AtBegining(self, newval):
+    def addAtHead(self, newval):
         NewHead = Node(newval)
         NewHead.next = self.head
         self.head = NewHead 
     
     
-    def AtTail(self, newval):
+    def addAtTail(self, newval):
         NewTail = Node(newval)
         if self.head is None:
             self.head = NewTail
@@ -41,31 +52,61 @@ class SinglyLinkedList:
         last.next = NewTail    
          
          
-    def InsertBetween(self, middle_node, newval):
-        if middle_node is None:
-            print("the mentioned node is absent")
-            return
+    def InsertAtIndex(self, target_index, newval):
+        headval = self.head
+        cur_index = 0
         NewNode = Node(newval)
-        NewNode.next = middle_node.next 
-        middle_node.next = NewNode
-       
-        
-    def RemoveNode(self, RemoveKey):
-        headval = self.head 
-        if headval is not None:
-            if headval.val == RemoveKey:
-                self.head = headval.next 
-                headval = None 
-                return 
-        while headval is not None:
-            if headval.val == RemoveKey:
-                break
-            prev = headval
-            headval = headval.next
-        if headval == None:
+        if target_index > self.get_count():
+            print('that index does not exist')
             return
-        prev.next = headval.next 
-        headval = None        
+        while headval is not None:
+            if cur_index == target_index:
+                NewNode.next = headval.next 
+                headval.next = NewNode
+                return
+            else:
+                # print('current node is not matching target index. moving to next node')
+                headval = headval.next
+                cur_index += 1
+        
+        
+    def RemoveAtIndex(self, remove_index):
+        if self.head is None:
+            return
+        if remove_index == 0:
+            self.head = self.head.next 
+            return self.head
+        if remove_index > self.get_count():
+            print('invalid index')
+            return
+        cur = self.head
+        prev = self.head 
+        temp = self.head
+        cur_index = 0
+        while cur is not None:
+            if cur_index == remove_index:
+                temp = cur.next
+                break
+            prev = cur
+            cur = cur.next   
+            cur_index += 1
+        prev.next = temp
+        return prev
+         
+        # if headval is not None:
+        #     if headval.val == RemoveKey:
+        #         self.head = headval.next 
+        #         headval = None 
+        #         return 
+        # while headval is not None:
+        #     if headval.val == RemoveKey:
+        #         break
+        #     prev = headval
+        #     headval = headval.next
+        # if headval == None:
+        #     return
+        # prev.next = headval.next 
+        # headval = None        
         
         
 
@@ -81,10 +122,12 @@ e2.next = e3
 
 #newlist.list_print()        
 
-newlist.AtBegining("Sun")
-newlist.AtTail("Thurs")
-newlist.AtTail("Sat")
-newlist.InsertBetween(newlist.head.next, "Fri")
-newlist.RemoveNode("Fri")
-newlist.list_print()
+newlist.addAtHead("Sun")
+newlist.addAtTail("Thurs")
+newlist.addAtTail("Sat")
+newlist.InsertAtIndex(4, "Fri")
+#newlist.RemoveNode("Fri")
+newlist.RemoveAtIndex(5)
 #newlist.get_index(2)
+newlist.list_print()
+print(f'the length of newlist is {newlist.get_count()}')
